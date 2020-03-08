c71f24913414f8b6ac6f30b985467797968c87bf
yukiiiiii <t0210yyyyyyyy@gmail.com>
2020-03-08 14:30:56 +0000

    validation

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,7 +16,6 @@
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    /*<td>{{ $task->id }}</td>*/
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
